fe6377c2cb0c64ee079637038d8be3cc86d63ec6
kanyan <kanyanyouth@gmail.com>
2022-04-28 21:55:52 -0400

    modify

---
+++ react-namelist/src/App.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+
+
+function App() {
+    const [users, setUsers] = useState([])
+    const addUserListHandler = (uName, uAge) => {
+        setUsers((prevUser) => {
+            return [...prevUser, {name: uName, age: uAge}]
+        })
+    }
+    return (
+        <div>
+            <AddUser onAddUser={addUserListHandler()}/>
+            <UsersList users={users}/>
+        </div>
+    );
+}
+
+export default App;
