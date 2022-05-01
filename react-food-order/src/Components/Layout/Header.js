caae88e3a52249e634ca3a42ebc364393409c552
kanyan <kanyanyouth@gmail.com>
2022-04-30 23:00:49 -0400

    modify

--- react-food-order/src/Components/Layout/Header.js
+++ react-food-order/src/Components/Layout/Header.js
@@ -4,12 +4,12 @@ import HeaderCardButton from "./HeaderCardButton";
 
 import classes from "./Header.module.css"
 
-const Header = () => {
+const Header = (props) => {
     return (
         <Fragment>
             <header className={classes.header}>
                 <h1>FancyOrder</h1>
-                <HeaderCardButton/>
+                <HeaderCardButton onClick={props.onShowCart}/>
             </header>
             <div className={classes['main-image']}>
                 <img src={mealsImage} alt='A table full of delicious food!'/>
--- react-food-order/src/Components/Layout/HeaderCardButton.js
+++ react-food-order/src/Components/Layout/HeaderCardButton.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, {useContext, useEffect, useState } from 'react';
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../Store/cart-context";
 import classes from './HeaderCardButton.module.css'
 
-const HeaderCardButton = () => {
+const HeaderCardButton = (props) => {
+    const [btnHighlighted, setBtnHighlighted] = useState(false)
+    const cartCtx = useContext(CartContext)
+
+    const {items} = cartCtx
+
+    const numberOfCartItems = items.reduce((curNumber, item)=>{
+        return curNumber + item.amount;
+    }, 0)
+
+    const btnClasses = `${classes.button} ${btnHighlighted ? classes.bump : ''}`
+
+    useEffect(()=>{
+        if (items.length === 0){
+            return;
+        }
+        setBtnHighlighted(true)
+        const timer = setTimeout(()=>{
+            setBtnHighlighted(false)
+        }, 300)
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [items])
+
     return (
-        <button className={classes.button}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon/>
             </span>
             <span>
                 Your Cart
             </span>
-            <span className={classes.badge}>3</span>
+            <span className={classes.badge}>{numberOfCartItems}</span>
         </button>
     );
 };
