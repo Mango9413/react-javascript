ef38f1ed574304941f5751188f414b6c1337e262
kanyan <kanyanyouth@gmail.com>
2022-05-03 23:02:56 -0400

    modify

--- react-redux/src/store/index.js
+++ react-redux/src/store/index.js
@@ -1,38 +1,41 @@
-import {createStore} from "redux";
+// import {createStore} from "redux";
+import { configureStore} from "@reduxjs/toolkit";
+import counterReducer from "./counter";
+import authReducer from "./auth"
 
-const initState = {
-    counter: 0,
-    showCounter: true
-}
-
-const counterReducer = (state=initState, action) => {
-    if (action.type === 'increment'){
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter
-        }
+const store = configureStore({
+    reducer: {
+        counter: counterReducer,
+        auth: authReducer,
     }
-    if (action.type === 'decrement'){
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter
-        }
-    }
-    if (action.type === 'increase'){
-        return {
-            counter: state.counter + action.amount,
-            showCounter: state.showCounter
-        }
-    }
-    if (action.type === 'toggle'){
-        return {
-            showCounter: !state.showCounter,
-            counter: state.counter
-        }
-    }
-    return initState
-}
-
-const store = createStore(counterReducer)
-
+})
 export default store
+// const counterReducer = (state=initState, action) => {
+//     if (action.type === 'increment'){
+//         return {
+//             counter: state.counter + 1,
+//             showCounter: state.showCounter
+//         }
+//     }
+//     if (action.type === 'decrement'){
+//         return {
+//             counter: state.counter - 1,
+//             showCounter: state.showCounter
+//         }
+//     }
+//     if (action.type === 'increase'){
+//         return {
+//             counter: state.counter + action.amount,
+//             showCounter: state.showCounter
+//         }
+//     }
+//     if (action.type === 'toggle'){
+//         return {
+//             showCounter: !state.showCounter,
+//             counter: state.counter
+//         }
+//     }
+//     return initState
+// }
+// const store = createStore(counterReducer)
+
